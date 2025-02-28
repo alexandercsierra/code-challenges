82cd6ad0540be1ff051fefdea0447a7e4d5cab15
alexandercsierra <alexandercsierra@gmail.com>
2025-02-28 11:38:47 -0500

    working solution and tests for reverseString

--- leetcode/dsaCourse/arraysAndStrings/twoPointers.js
+++ leetcode/dsaCourse/arraysAndStrings/twoPointers.js
@@ -1,5 +1,28 @@
-const reverseString = () => {
+// Write a function that reverses a string (array)
+// do this in place with O(1) memory
+
+const reverseString = (stringArr) => {
+    // if empty or length 1, nothing to reverse, return as-is
+    if (stringArr.length < 2) return stringArr
+
+    let pointer1 = 0;
+    let pointer2 = stringArr.length - 1 - pointer1
+
+    while (pointer1 < pointer2) {
+        const firstLetter = stringArr[pointer1];
+        const lastLetter = stringArr[pointer2];
+
+        stringArr[pointer1] = lastLetter;
+        stringArr[pointer2] = firstLetter
+
+        pointer1++;
+        pointer2--;
+    }
 
 }
 
+
 module.exports = { reverseString }
+
+
+
--- leetcode/dsaCourse/arraysAndStrings/twoPointers.test.js
+++ leetcode/dsaCourse/arraysAndStrings/twoPointers.test.js
@@ -1,10 +1,45 @@
 const { reverseString } = require('./twoPointers')
 
 describe('twoPointer problem tests', () => {
-    test('reverseString', () => {
-        const input = ["h", "e", "l", "l", "o"]
-        const expectedOutput = ['o', 'l', 'l', 'e', 'h']
 
-        expect(reverseString(input)).toMatchObject(expectedOutput)
+    describe('reverseString', () => {
+
+        test('reverses odd length string', () => {
+
+            const input = ["h", "e", "l", "l", "o"]
+            const expectedOutput = ['o', 'l', 'l', 'e', 'h']
+
+            reverseString(input)
+
+            expect(input).toMatchObject(expectedOutput)
+        })
+        test('reverses even length string', () => {
+
+            const input = ["f", "o", "o", "d"]
+            const expectedOutput = ['d', 'o', 'o', 'f']
+
+            reverseString(input)
+
+            expect(input).toMatchObject(expectedOutput)
+        })
+        test('handles empty array', () => {
+            const input = []
+            const expectedOutput = []
+
+            reverseString(input)
+
+            expect(input).toMatchObject(expectedOutput)
+        })
+        test('handles array of length 1', () => {
+            const input = ["a"]
+            const expectedOutput = ["a"]
+
+            reverseString(input)
+
+            expect(input).toMatchObject(expectedOutput)
+        })
     })
+
+
+
 })
