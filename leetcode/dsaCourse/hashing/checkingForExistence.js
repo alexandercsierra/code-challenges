a9c181ce4e6d99a1b87c0f8aa068ad62d66e3f96
alexandercsierra <alexandercsierra@gmail.com>
2025-03-05 13:39:47 -0500

    working solution for missingNumber, and updating template

--- leetcode/dsaCourse/hashing/checkingForExistence.js
+++ leetcode/dsaCourse/hashing/checkingForExistence.js
@@ -20,4 +20,50 @@ const checkIfPangramShort = (sentence) => {
 
 }
 
-module.exports = { checkIfPangram, checkIfPangramShort }
+// relies on sorting the array first
+// checks to see if current num is one more than last
+const missingNumber = (nums) => {
+
+    nums.sort((a, b) => a - b)
+    let missingNum = nums[nums.length - 1] + 1;
+
+    let lastNum = nums[0]
+    if (lastNum !== 0) return 0
+    for (let i = 1; i < nums.length; i++) {
+        const currNum = nums[i];
+        if (currNum !== lastNum + 1) return lastNum + 1;
+        lastNum = currNum
+    }
+
+    return missingNum
+}
+
+
+// fills an array of nulls, then fills in actual numbers
+// according to index one by one
+// whatever null still remains must be the missing number
+// unless they are all filled in which case it is the last value + 1
+const missingNumberArr = (nums) => {
+    const foundNums = Array(nums.length).fill(null)
+    nums.forEach(num => foundNums[num] = num)
+
+    const missing = foundNums.indexOf(null)
+    return missing === -1 ? nums.length : missing
+}
+
+
+// relies on math for calculating sum of all nums for this length of array
+// whatever is left from what should be the sum is the missing number
+const missingNumberSum = (nums) => {
+    const length = nums.length;
+    // math formula for sum of ints up to length
+    const expectedSum = (length * (length + 1)) / 2
+    const sum = nums.reduce((a, b) => a + b, 0)
+    return expectedSum - sum
+
+}
+
+module.exports = {
+    checkIfPangram, checkIfPangramShort, missingNumber, missingNumberArr, missingNumberSum
+
+}
--- leetcode/dsaCourse/hashing/checkingForExistence.test.js
+++ leetcode/dsaCourse/hashing/checkingForExistence.test.js
@@ -1,4 +1,4 @@
-const { checkIfPangram, checkIfPangramShort } = require('./checkingForExistence')
+const { checkIfPangram, checkIfPangramShort, missingNumber, missingNumberArr, missingNumberSum } = require('./checkingForExistence')
 
 
 const testCasesPangram = [
@@ -9,6 +9,29 @@ const testCasesPangram = [
     {
         input: 'leetcode',
         output: false
+    }
+]
+
+const testCassesMissingNum = [
+    {
+        input: [3, 0, 1],
+        output: 2
+    },
+    {
+        input: [0, 1],
+        output: 2
+    },
+    {
+        input: [9, 6, 4, 2, 3, 5, 7, 0, 1],
+        output: 8
+    },
+    {
+        input: [1],
+        output: 0
+    },
+    {
+        input: [1, 0],
+        output: 2
     }
 ]
 
@@ -23,5 +46,22 @@ describe('checkIfPangram', () => {
 describe('checkIfPangramShort', () => {
     test.each(testCasesPangram)('$input => $output', ({ input, output }) => {
         expect(checkIfPangramShort(input)).toBe(output)
+    })
+})
+
+describe('missingNumber', () => {
+    test.each(testCassesMissingNum)('$input => $output', ({ input, output }) => {
+        expect(missingNumber(input)).toBe(output)
+    })
+})
+describe('missingNumberArr', () => {
+    test.each(testCassesMissingNum)('$input => $output', ({ input, output }) => {
+        expect(missingNumberArr(input)).toBe(output)
+    })
+})
+
+describe('missingNumberSum', () => {
+    test.each(testCassesMissingNum)('$input => $output', ({ input, output }) => {
+        expect(missingNumberSum(input)).toBe(output)
     })
 })
