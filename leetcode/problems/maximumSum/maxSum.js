064508b9b144cff4ec60d405e9b890ee90175126
alexandercsierra <alexandercsierra@gmail.com>
2025-03-11 14:56:04 -0400

    additional solution for maxSum

--- leetcode/problems/maximumSum/maxSum.js
+++ leetcode/problems/maximumSum/maxSum.js
@@ -28,4 +28,27 @@ function maxSum(nums) {
     return maxSum
 };
 
-module.exports = { maxSum }
+// since the problem only calls for us to compare two numbers
+// we don't have to store them all
+// for ex if we had [18, 43, 36, 27, 7]
+// instead of storing 18, 36, and 27 under 9
+// we can update the max 18 + 36 = 54
+// then take the bigger of those two nums, 36 and discard 18
+// then it will be 36 + 27 => new max is 63
+const maxSumNoSort = (nums) => {
+    let digitMap = new Map()
+    let maxSum = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        const digitSum = nums[i].toString().split("").reduce((acc, curr) => Number(curr) + Number(acc), 0)
+        if (digitMap.has(digitSum)) {
+            maxSum = Math.max(maxSum, nums[i] + digitMap.get(digitSum))
+        }
+        const biggerNum = Math.max(digitMap.get(digitSum) || 0, nums[i])
+        digitMap.set(digitSum, biggerNum)
+    }
+
+    return maxSum
+}
+
+module.exports = { maxSum, maxSumNoSort }
--- leetcode/problems/maximumSum/maxSum.test.js
+++ leetcode/problems/maximumSum/maxSum.test.js
@@ -1,4 +1,4 @@
-const { maxSum } = require('./maxSum')
+const { maxSum, maxSumNoSort } = require('./maxSum')
 
 const testCases = [{
     input: [18, 43, 36, 13, 7],
@@ -7,11 +7,20 @@ const testCases = [{
 {
     input: [10, 12, 19, 14],
     output: -1
+},
+{
+    input: [18, 43, 36, 27, 7],
+    output: 63
 }
 ]
 
 describe('maxSum', () => {
     test.each(testCases)('$input => $output', ({ input, output }) => {
         expect(maxSum(input)).toBe(output)
+    })
+})
+describe('maxSumNoSort', () => {
+    test.each(testCases)('$input => $output', ({ input, output }) => {
+        expect(maxSumNoSort(input)).toBe(output)
     })
 })
