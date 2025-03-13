7e5a38b5c33e043d63a239b3de7774ef2ae60dfb
alexandercsierra <alexandercsierra@gmail.com>
2025-03-13 12:15:11 -0400

    add utils for linked list tests

--- leetcode/dsaCourse/linkedLists/fastAndSlowPointers.test.js
+++ leetcode/dsaCourse/linkedLists/fastAndSlowPointers.test.js
@@ -1,25 +1,6 @@
 const { middleNode, deleteDuplicates } = require('./fastAndSlowPointers')
+const { createLinkedList } = require('../../utls/linkedLists')
 
-class ListNode {
-    constructor(val, next) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
-}
-
-const createLinkedList = (arr) => {
-
-    const head = new ListNode(arr[0]);
-    let node = head;
-
-    for (let i = 1; i < arr.length; i++) {
-        node.next = new ListNode(arr[i])
-        node = node.next;
-    }
-
-    return head
-
-}
 
 const testCasesMiddle = [
     {
--- leetcode/problems/swapNodesInPairs/swapNodes.test.js
+++ leetcode/problems/swapNodesInPairs/swapNodes.test.js
@@ -1,25 +1,5 @@
 const { swapPairs } = require('./swapNodes')
-
-class ListNode {
-    constructor(val, next) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
-}
-
-const createLinkedList = (arr) => {
-
-    const head = new ListNode(arr[0]);
-    let node = head;
-
-    for (let i = 1; i < arr.length; i++) {
-        node.next = new ListNode(arr[i])
-        node = node.next;
-    }
-
-    return head
-
-}
+const { createLinkedList } = require('../../utls/linkedLists')
 
 const testCases = [
     {
